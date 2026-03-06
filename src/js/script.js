2c529382b19b4fbf72a0e233f578f0484e3e8233
Eduardo Moraes <eduzinho2118@gmail.com>
2026-03-05 22:48:28 -0300

    implementei agenda no site

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+//menu mobile
 function toggleMenu() {
     const menu = document.getElementById('mobile-menu');
     menu.classList.toggle('hidden');
@@ -5,3 +6,15 @@ function toggleMenu() {
 
 const menuBtn = document.getElementById('mobile-menu-btn');
 menuBtn.addEventListener('click', toggleMenu);
+
+
+//acessar agenda
+function acessarAgenda(){
+    const acessAgenda = document.getElementById('acess-agenda');
+
+    acessAgenda.addEventListener('click', ()=>{
+        
+        window.open('https://agenda-ibm-ipsep.netlify.app/', '_blank')
+    })
+}
+acessarAgenda();
